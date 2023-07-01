5d7a15fc60e45cff0e9bb19245dc9661af20125f
Sourabh Kumar <20010001030@gateway.edu.in>
2023-07-02 02:33:54 +0530

    created viewModel

--- app/src/main/java/com/example/amphibians/ui/screens/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibianViewModel.kt
@@ -4,28 +4,55 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibians.AmphibianPhotosApplication
+import com.example.amphibians.data.AmphibianPhotoRepository
+import com.example.amphibians.network.AmphibianPhoto
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 /**
  * UI state for the [HomeScreen].
  */
-sealed interface AmphibialUiState {
-    data class Success(val photos: List<AmphibianPhoto>) : AmphibialUiState
-    object Loading : AmphibialUiState
-    object Error : AmphibialUiState
+sealed interface AmphibianUiState {
+    data class Success(val photos: List<AmphibianPhoto>) : AmphibianUiState
+    object Loading : AmphibianUiState
+    object Error : AmphibianUiState
 }
 
-class AmphibianViewModel() : ViewModel() {
+class AmphibianViewModel(private val amphibianPhotoRepository: AmphibianPhotoRepository) :
+    ViewModel() {
 
     //stores the current state of the UI
-    var amphibialUiState: AmphibialUiState by mutableStateOf(AmphibialUiState.Loading)
+    var amphibianUiState: AmphibianUiState by mutableStateOf(AmphibianUiState.Loading)
         private set
 
-    fun getAmphibiansDetails(){
+    fun getAmphibiansDetails() {
         viewModelScope.launch {
-            amphibialUiState = try {
-                amphibialUiState.Success()
+            amphibianUiState = try {
+                AmphibianUiState.Success(amphibianPhotoRepository.getAmphibianDetails())
+            } catch (e: IOException) {
+                AmphibianUiState.Error
+            } catch (e: HttpException) {
+                AmphibianUiState.Error
+            }
+        }
+    }
+
+    /**
+     * Factory for [AmphibianViewModel] that takes [AmphibianPhotoRepository] as a dependency
+     */
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as AmphibianPhotosApplication)
+                val amphibianPhotoRepository = application.container.amphibianPhotoRepository
+                AmphibianViewModel(amphibianPhotoRepository = amphibianPhotoRepository)
             }
         }
     }
