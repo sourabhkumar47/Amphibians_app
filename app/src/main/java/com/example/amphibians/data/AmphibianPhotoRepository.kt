1ce66572e9682cc3b7df7d0a93251fcea17adb13
Sourabh Kumar <20010001030@gateway.edu.in>
2023-07-02 02:11:13 +0530

    created data layer

--- app/src/main/java/com/example/amphibians/data/AmphibianPhotoRepository.kt
+++ app/src/main/java/com/example/amphibians/data/AmphibianPhotoRepository.kt
@@ -1,2 +1,16 @@
 package com.example.amphibians.data
 
+import com.example.amphibians.network.AmphibianApiService
+import com.example.amphibians.network.AmphibianPhoto
+
+interface AmphibianPhotoRepository {
+    //fetch list of MarsPhoto from API
+    suspend fun getAmphibianDetails(): List<AmphibianPhoto>
+}
+
+class NetworkAmphibianPhotoRepository(
+    private val amphibianApiService: AmphibianApiService
+) : AmphibianPhotoRepository {
+    override suspend fun getAmphibianDetails(): List<AmphibianPhoto> =
+        amphibianApiService.getAmphibians()
+}
--- app/src/main/java/com/example/amphibians/data/AppContainer.kt
+++ app/src/main/java/com/example/amphibians/data/AppContainer.kt
@@ -1,2 +1,47 @@
 package com.example.amphibians.data
 
+import com.example.amphibians.network.AmphibianApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+//Dependency Injection
+
+interface AppContainer {
+    val amphibianPhotoRepository: AmphibianPhotoRepository
+}
+
+/**
+ * Implementation for the Dependency Injection container at the application level.
+ *
+ * Variables are initialized lazily and the same instance is shared across the whole app.
+ */
+
+class DefaultAppContainer : AppContainer {
+    private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com/amphibians/"
+
+    /**
+     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+     */
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+
+    /**
+     * Retrofit service object for creating api calls
+     */
+    private val retrofitService: AmphibianApiService by lazy {
+        retrofit.create(AmphibianApiService::class.java)
+    }
+
+    /**
+     * DI implementation for Mars photos repository
+     */
+    override val amphibianPhotoRepository: AmphibianPhotoRepository by lazy {
+        NetworkAmphibianPhotoRepository(retrofitService)
+    }
+}
+
