107bf21d7a71f900974586cd43090b98179ec818
Sourabh Kumar <20010001030@gateway.edu.in>
2023-07-05 02:24:25 +0530

    added amphibianCard

--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -3,12 +3,9 @@ package com.example.amphibians.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -17,9 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.amphibians.R
 import com.example.amphibians.network.Amphibians
 
@@ -55,7 +55,26 @@ fun AmphibianPhotoCard(amphibians: Amphibians, modifier: Modifier = Modifier) {
         shape = RoundedCornerShape(8.dp)
     ) {
         Column {
-            Text(stringResource(R.string.amphibian_title))
+            Text(
+                text = stringResource(R.string.amphibian_title, amphibians.name, amphibians.type),
+                modifier = modifier
+                    .fillMaxWidth()
+            )
+
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(amphibians.img_src)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img)
+            )
+
+            Text(
+                text = amphibians.description
+            )
+
         }
     }
 }
@@ -77,20 +96,6 @@ fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier) {
         )
         Button(onClick = retryAction) {
             Text(text = "Retry")
-        }
-    }
-}
-
-
-@Composable
-fun PhotoGrid(modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(1),
-        modifier = modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        item {
-            AmphibianPhotoCard()
         }
     }
 }
